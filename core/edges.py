b728ff0f680eff6ef67c86329794687d99dbf304
Preston Rasmussen <109292228+prasmussen15@users.noreply.github.com>
2024-08-15 11:04:57 -0400

    renaming and add indices (#3)
    rename and add indices

--- core/edges.py
+++ core/edges.py
@@ -11,10 +11,10 @@ logger = logging.getLogger(__name__)
 
 
 class Edge(BaseModel, ABC):
-    uuid: Field(default_factory=lambda: uuid1().hex)
+    uuid: str = Field(default_factory=lambda: uuid1().hex)
     source_node: Node
     target_node: Node
-    transaction_from: datetime
+    created_at: datetime
 
     @abstractmethod
     async def save(self, driver: AsyncDriver): ...
@@ -25,14 +25,14 @@ class EpisodicEdge(Edge):
         result = await driver.execute_query(
             """
         MATCH (episode:Episodic {uuid: $episode_uuid}) 
-        MATCH (node:Semantic {uuid: $semantic_uuid}) 
+        MATCH (node:Entity {uuid: $entity_uuid}) 
         MERGE (episode)-[r:MENTIONS {uuid: $uuid}]->(node)
-        SET r = {uuid: $uuid, transaction_from: $transaction_from}
+        SET r = {uuid: $uuid, created_at: $created_at}
         RETURN r.uuid AS uuid""",
             episode_uuid=self.source_node.uuid,
-            semantic_uuid=self.target_node.uuid,
+            entity_uuid=self.target_node.uuid,
             uuid=self.uuid,
-            transaction_from=self.transaction_from,
+            created_at=self.created_at,
         )
 
         logger.info(f"Saved edge to neo4j: {self.uuid}")
@@ -44,14 +44,14 @@ class EpisodicEdge(Edge):
 #  Right now we have all edge nodes as type RELATES_TO
 
 
-class SemanticEdge(Edge):
+class EntityEdge(Edge):
     name: str
     fact: str
-    fact_embedding: list[int] = None
-    episodes: list[str] = None  # list of episodes that reference these semantic edges
-    transaction_to: datetime = None  # datetime of when the node was invalidated
-    valid_from: datetime = None  # datetime of when the fact became true
-    valid_to: datetime = None  # datetime of when the fact stopped being true
+    fact_embedding: list[float] = None
+    episodes: list[str] = None  # list of episode ids that reference these entity edges
+    expired_at: datetime = None  # datetime of when the node was invalidated
+    valid_at: datetime = None  # datetime of when the fact became true
+    invalid_at: datetime = None  # datetime of when the fact stopped being true
 
     def generate_embedding(self, embedder, model="text-embedding-3-large"):
         text = self.fact.replace("\n", " ")
@@ -63,12 +63,12 @@ class SemanticEdge(Edge):
     async def save(self, driver: AsyncDriver):
         result = await driver.execute_query(
             """
-        MATCH (source:Semantic {uuid: $source_uuid}) 
-        MATCH (target:Semantic {uuid: $target_uuid}) 
+        MATCH (source:Entity {uuid: $source_uuid}) 
+        MATCH (target:Entity {uuid: $target_uuid}) 
         MERGE (source)-[r:RELATES_TO {uuid: $uuid}]->(target)
         SET r = {uuid: $uuid, name: $name, fact: $fact, fact_embedding: $fact_embedding, 
-        episodes: $episodes, transaction_from: $transaction_from, transaction_to: $transaction_to, 
-        valid_from: $valid_from, valid_to: $valid_to}
+        episodes: $episodes, created_at: $created_at, expired_at: $expired_at, 
+        valid_at: $valid_at, invalid_at: $invalid_at}
         RETURN r.uuid AS uuid""",
             source_uuid=self.source_node.uuid,
             target_uuid=self.target_node.uuid,
@@ -77,10 +77,10 @@ class SemanticEdge(Edge):
             fact=self.fact,
             fact_embedding=self.fact_embedding,
             episodes=self.episodes,
-            transaction_from=self.transaction_from,
-            transaction_to=self.transaction_to,
-            valid_from=self.valid_from,
-            valid_to=self.valid_to,
+            created_at=self.created_at,
+            expired_at=self.expired_at,
+            valid_at=self.valid_at,
+            invalid_at=self.invalid_at,
         )
 
         logger.info(f"Saved Node to neo4j: {self.uuid}")
--- core/graphiti.py
+++ core/graphiti.py
@@ -1,11 +1,11 @@
 import asyncio
 from datetime import datetime
 import logging
-from typing import Callable, Tuple
+from typing import Callable, Tuple, LiteralString
 from neo4j import AsyncGraphDatabase
 
-from core.nodes import SemanticNode, EpisodicNode, Node
-from core.edges import SemanticEdge, Edge
+from core.nodes import EntityNode, EpisodicNode, Node
+from core.edges import EntityEdge, Edge
 from core.utils import bfs, similarity_search, fulltext_search, build_episodic_edges
 
 logger = logging.getLogger(__name__)
@@ -31,6 +31,9 @@ class Graphiti:
     ):
         self.driver = AsyncGraphDatabase.driver(uri, auth=(user, password))
         self.database = "neo4j"
+
+        self.build_indices()
+
         if llm_config:
             self.llm_config = llm_config
         else:
@@ -38,6 +41,40 @@ class Graphiti:
 
     def close(self):
         self.driver.close()
+
+    async def build_indices(self):
+        index_queries: list[LiteralString] = [
+            "CREATE INDEX name_entity_index IF NOT EXISTS FOR (n:Entity) ON (n.name)",
+            "CREATE INDEX created_at_entity_index IF NOT EXISTS FOR (n:Entity) ON (n.created_at)",
+            "CREATE INDEX created_at_episodic_index IF NOT EXISTS FOR (n:Episodic) ON (n.created_at)",
+            "CREATE INDEX valid_at_episodic_index IF NOT EXISTS FOR (n:Episodic) ON (n.valid_at)",
+            "CREATE INDEX name_edge_index IF NOT EXISTS FOR ()-[r:RELATES_TO]-() ON (r.name)",
+            "CREATE INDEX created_at_edge_index IF NOT EXISTS FOR ()-[r:RELATES_TO]-() ON (r.created_at)",
+            "CREATE INDEX expired_at_edge_index IF NOT EXISTS FOR ()-[r:RELATES_TO]-() ON (r.expired_at)",
+            "CREATE INDEX valid_at_edge_index IF NOT EXISTS FOR ()-[r:RELATES_TO]-() ON (r.valid_at)",
+            "CREATE INDEX invalid_at_edge_index IF NOT EXISTS FOR ()-[r:RELATES_TO]-() ON (r.invalid_at)",
+        ]
+        # Add the range indices
+        for query in index_queries:
+            await self.driver.execute_query(query)
+
+        # Add the entity indices
+        await self.driver.execute_query(
+            """
+            CREATE FULLTEXT INDEX name_and_summary IF NOT EXISTS FOR (n:Entity) ON EACH [n.name, n.summary]
+            """
+        )
+
+        await self.driver.execute_query(
+            """
+            CREATE VECTOR INDEX fact_embedding IF NOT EXISTS
+            FOR ()-[r:RELATES_TO]-() ON (r.fact_embedding)
+            OPTIONS {indexConfig: {
+             `vector.dimensions`: 1024,
+             `vector.similarity_function`: 'cosine'
+            }}
+            """
+        )
 
     async def retrieve_episodes(
         self, last_n: int, sources: list[str] | None = "messages"
@@ -48,8 +85,9 @@ class Graphiti:
     # Utility function, to be removed from this class
     async def clear_data(self): ...
 
-    async def search(self, query: str, config) -> (
-            list)[Tuple[SemanticNode, list[SemanticEdge]]]:
+    async def search(
+        self, query: str, config
+    ) -> (list)[Tuple[EntityNode, list[EntityEdge]]]:
         (vec_nodes, vec_edges) = similarity_search(query, embedder)
         (text_nodes, text_edges) = fulltext_search(query)
 
@@ -64,8 +102,9 @@ class Graphiti:
 
         return [(node, edges)], episodes
 
-    async def get_relevant_schema(self, episode: EpisodicNode, previous_episodes: list[EpisodicNode]) -> (
-            list)[Tuple[SemanticNode, list[SemanticEdge]]]:
+    async def get_relevant_schema(
+        self, episode: EpisodicNode, previous_episodes: list[EpisodicNode]
+    ) -> list[Tuple[EntityNode, list[EntityEdge]]]:
         pass
 
     # Call llm with the specified messages, and return the response
@@ -76,10 +115,10 @@ class Graphiti:
     async def extract_new_edges(
         self,
         episode: EpisodicNode,
-        new_nodes: list[SemanticNode],
+        new_nodes: list[EntityNode],
         relevant_schema: dict[str, any],
         previous_episodes: list[EpisodicNode],
-    ) -> list[SemanticEdge]: ...
+    ) -> list[EntityEdge]: ...
 
     # Extract new nodes from the episode
     async def extract_new_nodes(
@@ -87,14 +126,14 @@ class Graphiti:
         episode: EpisodicNode,
         relevant_schema: dict[str, any],
         previous_episodes: list[EpisodicNode],
-    ) -> list[SemanticNode]: ...
+    ) -> list[EntityNode]: ...
 
     # Invalidate edges that are no longer valid
     async def invalidate_edges(
         self,
         episode: EpisodicNode,
-        new_nodes: list[SemanticNode],
-        new_edges: list[SemanticEdge],
+        new_nodes: list[EntityNode],
+        new_edges: list[EntityEdge],
         relevant_schema: dict[str, any],
         previous_episodes: list[EpisodicNode],
     ): ...
@@ -137,7 +176,7 @@ class Graphiti:
             await asyncio.gather(*[node.save(self.driver) for node in nodes])
             await asyncio.gather(*[edge.save(self.driver) for edge in edges])
             for node in nodes:
-                if isinstance(node, SemanticNode):
+                if isinstance(node, EntityNode):
                     await node.update_summary(self.driver)
             if success_callback:
                 await success_callback(episode)
