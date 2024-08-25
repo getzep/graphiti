38d1d891b0e199163a04a65eaaa31d9cafac851a
prestonrasmussen <prasmuss15@gmail.com>
2024-08-25 09:56:49 -0400

    remove r

--- examples/ecommerce/allbirds_runner.py
+++ examples/ecommerce/allbirds_runner.py
@@ -47,7 +47,7 @@ async def main(use_bulk: bool = True):
     client = Graphiti(neo4j_uri, neo4j_user, neo4j_password)
     await clear_data(client.driver)
     await client.build_indices_and_constraints()
-    with open('allbirds_products.json', 'r') as products_file:
+    with open('allbirds_products.json') as products_file:
         products = json.load(products_file)['products']
 
     if not use_bulk:
