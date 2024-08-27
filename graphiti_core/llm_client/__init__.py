3a3c4782ae5539487743e17ead80c9f3aeecba8f
Pavlo Paliychuk <pavlo.paliychuk.ca@gmail.com>
2024-08-27 16:03:08 -0400

    chore: Move anthropic to dev deps, remove anthropic and groq clients from __init__ (#61)

--- graphiti_core/llm_client/__init__.py
+++ graphiti_core/llm_client/__init__.py
@@ -1,7 +1,5 @@
-from .anthropic_client import AnthropicClient
 from .client import LLMClient
 from .config import LLMConfig
-from .groq_client import GroqClient
 from .openai_client import OpenAIClient
 
-__all__ = ['LLMClient', 'OpenAIClient', 'LLMConfig', 'AnthropicClient', 'GroqClient']
+__all__ = ['LLMClient', 'OpenAIClient', 'LLMConfig']
