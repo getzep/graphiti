a73f9443d67ae7749ce0ecef07f0bdd3d89dc4d4
paulpaliychuk <pavlo.paliychuk.ca@gmail.com>
2024-09-09 21:29:04 -0400

    chore: Fix linter

--- server/graph_service/dto/ingest.py
+++ server/graph_service/dto/ingest.py
@@ -12,4 +12,4 @@ class AddEntityNodeRequest(BaseModel):
     uuid: str = Field(..., description='The uuid of the node to add')
     group_id: str = Field(..., description='The group id of the node to add')
     name: str = Field(..., description='The name of the node to add')
-    summary: str | None = Field(None, description='The summary of the node to add')
+    summary: str = Field(default='', description='The summary of the node to add')
