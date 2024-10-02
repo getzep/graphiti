c887170717bb9e44789fd8c685b404d2e200fedd
prestonrasmussen <prasmuss15@gmail.com>
2024-10-02 13:10:16 -0400

    test escape characters

---
+++ tests/helpers_test.py
@@ -0,0 +1,14 @@
+import pytest
+
+from graphiti_core.helpers import lucene_sanitize
+
+
+def test_lucene_sanitize():
+    # Call the function with test data
+    queries = [('This has every secape character + - && || ! ( ) { } [ ] ^ " ~ * ? : \\ /',
+                'This has every secape character \+ \- \&\& \|\| \! \( \) \{ \} \[ \] \^ \\" \~ \* \? \: \\\ \/'),
+               ('This has no escape characters', 'This has no escape characters')]
+
+    for query, assert_result in queries:
+        result = lucene_sanitize(query)
+        assert assert_result == result
