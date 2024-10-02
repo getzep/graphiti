c2c3bf513b83da20c20c2c156531f6c73b346867
prestonrasmussen <prasmuss15@gmail.com>
2024-10-02 15:12:40 -0400

    format

--- tests/helpers_test.py
+++ tests/helpers_test.py
@@ -5,9 +5,13 @@ from graphiti_core.helpers import lucene_sanitize
 
 def test_lucene_sanitize():
     # Call the function with test data
-    queries = [('This has every secape character + - && || ! ( ) { } [ ] ^ " ~ * ? : \\ /',
-                'This has every secape character \+ \- \&\& \|\| \! \( \) \{ \} \[ \] \^ \\" \~ \* \? \: \\\ \/'),
-               ('This has no escape characters', 'This has no escape characters')]
+    queries = [
+        (
+            'This has every secape character + - && || ! ( ) { } [ ] ^ " ~ * ? : \\ /',
+            'This has every secape character \+ \- \&\& \|\| \! \( \) \{ \} \[ \] \^ \\" \~ \* \? \: \\\ \/',
+        ),
+        ('This has no escape characters', 'This has no escape characters'),
+    ]
 
     for query, assert_result in queries:
         result = lucene_sanitize(query)
