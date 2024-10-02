63380e0dc73ffd970c2ab70411b5a961be53c15e
prestonrasmussen <prasmuss15@gmail.com>
2024-10-02 11:35:28 -0400

    update lucene sanitizer

--- examples/ecommerce/runner.py
+++ examples/ecommerce/runner.py
@@ -80,6 +80,7 @@ async def add_messages(client: Graphiti):
             source=EpisodeType.message,
             reference_time=datetime.now(),
             source_description='Shoe conversation',
+            group_id='500'
         )
 
 
@@ -117,6 +118,7 @@ async def ingest_products_data(client: Graphiti):
             episode.source_description,
             episode.reference_time,
             episode.source,
+            group_id='500'
         )
 
 
