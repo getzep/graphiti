845b37c1a98dcb87072657ff02128a67b9ec5114
Preston Rasmussen <109292228+prasmussen15@users.noreply.github.com>
2024-10-03 10:08:30 -0400

    test escape characters (#171)
    * test escape characters
    
    * format
    
    * tests
    
    * run tests
    
    * copyright

---
+++ tests/helpers_test.py
@@ -0,0 +1,38 @@
+"""
+Copyright 2024, Zep Software, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import pytest
+
+from graphiti_core.helpers import lucene_sanitize
+
+
+def test_lucene_sanitize():
+    # Call the function with test data
+    queries = [
+        (
+            'This has every secape character + - && || ! ( ) { } [ ] ^ " ~ * ? : \\ /',
+            'This has every secape character \+ \- \&\& \|\| \! \( \) \{ \} \[ \] \^ \\" \~ \* \? \: \\\ \/',
+        ),
+        ('This has no escape characters', 'This has no escape characters'),
+    ]
+
+    for query, assert_result in queries:
+        result = lucene_sanitize(query)
+        assert assert_result == result
+
+
+if __name__ == '__main__':
+    pytest.main([__file__])
