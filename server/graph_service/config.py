0f70eedaadbde007ff99787f198e6d8a9c704d1e
Arno <bahdaxxzm@gmail.com>
2024-10-09 00:03:55 +0800

    feat: support switching LLM providers and embedding providers through configuration file
    fix typecheck

--- server/graph_service/config.py
+++ server/graph_service/config.py
@@ -1,5 +1,5 @@
 from functools import lru_cache
-from typing import Annotated
+from typing import Annotated, Literal
 
 from fastapi import Depends
 from pydantic import Field
@@ -7,10 +7,19 @@ from pydantic_settings import BaseSettings, SettingsConfigDict  # type: ignore
 
 
 class Settings(BaseSettings):
-    openai_api_key: str
+    openai_api_key: str | None = Field(None)
     openai_base_url: str | None = Field(None)
+    
+    groq_api_key: str | None = Field(None)
+    anthropic_api_key: str | None = Field(None)
+    voyage_api_key: str | None = Field(None)
+
+    llm_provider: Literal['openai', 'groq', 'anthropic'] | None = Field(None)
     model_name: str | None = Field(None)
+    
+    embedding_provider: Literal['openai', 'voyage'] | None = Field(None)
     embedding_model_name: str | None = Field(None)
+
     neo4j_uri: str
     neo4j_user: str
     neo4j_password: str
