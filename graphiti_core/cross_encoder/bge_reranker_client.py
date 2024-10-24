544f9e3fba13424faa17a27c733db8822cbd3baa
Pavlo Paliychuk <paul@getzep.com>
2024-10-24 11:36:10 -0400

    chore: Set up cross encoder client (#201)
    * chore: Set up cross encoder client
    
    * fix: deps
    
    * chore: move voyage to dev deps

---
+++ graphiti_core/cross_encoder/bge_reranker_client.py
@@ -0,0 +1,45 @@
+"""
+Copyright 2024, Zep Software, Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import asyncio
+from typing import List, Tuple
+
+from sentence_transformers import CrossEncoder
+
+from graphiti_core.cross_encoder.client import CrossEncoderClient
+
+
+class BGERerankerClient(CrossEncoderClient):
+    def __init__(self):
+        self.model = CrossEncoder('BAAI/bge-reranker-v2-m3')
+
+    async def rank(self, query: str, passages: List[str]) -> List[Tuple[str, float]]:
+        if not passages:
+            return []
+
+        input_pairs = [[query, passage] for passage in passages]
+
+        # Run the synchronous predict method in an executor
+        loop = asyncio.get_running_loop()
+        scores = await loop.run_in_executor(None, self.model.predict, input_pairs)
+
+        ranked_passages = sorted(
+            [(passage, float(score)) for passage, score in zip(passages, scores)],
+            key=lambda x: x[1],
+            reverse=True,
+        )
+
+        return ranked_passages
