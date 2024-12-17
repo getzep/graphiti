23d6cabdd2ce684e5ddbba205faf67bf42fa2d60
prestonrasmussen <prasmuss15@gmail.com>
2024-12-17 11:12:19 -0500

    remove unused imports

--- graphiti_core/search/search.py
+++ graphiti_core/search/search.py
@@ -14,7 +14,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-import asyncio
 import logging
 from collections import defaultdict
 from time import time
