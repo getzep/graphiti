290788928011895f7d7aa7eec02a446e4fa70efe
prestonrasmussen <prasmuss15@gmail.com>
2025-04-20 14:52:04 -0400

    entity type updates

--- graphiti_core/utils/maintenance/node_operations.py
+++ graphiti_core/utils/maintenance/node_operations.py
@@ -306,7 +306,7 @@ async def resolve_extracted_node(
 
     # Prepare context for LLM
     existing_nodes_context = [
-        {'uuid': node.uuid, 'name': node.name, 'summary': node.summary}.update(node.attributes)
+        {**{'uuid': node.uuid, 'name': node.name, 'summary': node.summary}, **node.attributes}
         for node in existing_nodes
     ]
 
@@ -408,7 +408,7 @@ async def resolve_extracted_node(
                 if new_attributes.get(attribute_name) is None:
                     new_attributes[attribute_name] = attribute_value
             node.attributes = new_attributes
-            node.labels = set(existing_node.labels + existing_node.labels)
+            node.labels = list(set(existing_node.labels + existing_node.labels))
 
             uuid_map[extracted_node.uuid] = existing_node.uuid
 
