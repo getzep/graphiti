003bae5fe89a421e46674b6769210327a95df796
Yi-Ting Chiu <mytim710@gmail.com>
2025-05-03 23:03:52 -0700

    Simplifying imports for llm_client and cross_encoder
    Why?
    - it doesn't make sense to only expose OpenAIClient and not OpenAIGenericClient, AnthropicClient, GeminiClient, and GroqClient. It caused confusions.
    - Similar problem happens in cross_encoder as well, so I fixed it as well.
    
    Fix:
    - put them into `__init__.py`

--- graphiti_core/llm_client/__init__.py
+++ graphiti_core/llm_client/__init__.py
@@ -1,6 +1,19 @@
+from .anthropic_client import AnthropicClient
 from .client import LLMClient
 from .config import LLMConfig
 from .errors import RateLimitError
+from .gemini_client import GeminiClient
+from .groq_client import GroqClient
 from .openai_client import OpenAIClient
+from .openai_generic_client import OpenAIGenericClient
 
-__all__ = ['LLMClient', 'OpenAIClient', 'LLMConfig', 'RateLimitError']
+__all__ = [
+    'LLMClient',
+    'OpenAIClient',
+    'OpenAIGenericClient',
+    'AnthropicClient',
+    'GeminiClient',
+    'GroqClient',
+    'LLMConfig',
+    'RateLimitError',
+]
