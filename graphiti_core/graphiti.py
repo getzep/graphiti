c684e9ba6b4027064b9a0cf0158074b90a5dc6a1
John Nieves <karn09@gmail.com>
2025-06-15 00:25:01 -0400

    Refactor driver initialization to use GraphDriverFactory for improved flexibility

--- graphiti_core/graphiti.py
+++ graphiti_core/graphiti.py
@@ -25,7 +25,7 @@ from typing_extensions import LiteralString
 from graphiti_core.cross_encoder.client import CrossEncoderClient
 from graphiti_core.cross_encoder.openai_reranker_client import OpenAIRerankerClient
 from graphiti_core.driver.driver import GraphDriver
-from graphiti_core.driver.neo4j_driver import Neo4jDriver
+from graphiti_core.driver.factory import GraphDriverFactory
 from graphiti_core.edges import EntityEdge, EpisodicEdge
 from graphiti_core.embedder import EmbedderClient, OpenAIEmbedder
 from graphiti_core.graphiti_types import GraphitiClients
@@ -140,7 +140,7 @@ class Graphiti:
         Graphiti if you're using the default OpenAIClient.
         """
 
-        self.driver = graph_driver if graph_driver else Neo4jDriver(uri, user, password)
+        self.driver = graph_driver if graph_driver else GraphDriverFactory.get_driver(uri, user, password)
 
         self.database = DEFAULT_DATABASE
         self.store_raw_episode_content = store_raw_episode_content
