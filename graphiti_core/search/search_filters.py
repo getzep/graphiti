eb3b48d91218d887ed3ef0824174e0aac45f9fc5
prestonrasmussen <prasmuss15@gmail.com>
2025-12-08 10:43:37 -0500

    update

--- graphiti_core/search/search_filters.py
+++ graphiti_core/search/search_filters.py
@@ -43,7 +43,9 @@ class DateFilter(BaseModel):
 
 class PropertyFilter(BaseModel):
     property_name: str = Field(description='Property name')
-    property_value: str = Field(description='Value you wnt to match on for the property')
+    property_value: str | int | float | None = Field(
+        description='Value you want to match on for the property'
+    )
     comparison_operator: ComparisonOperator = Field(
         description='Comparison operator for the property'
     )
